5b5c3ef4c1affdf3c164bf0626a909dcf246a808
Mehdi Aiyad <82720736+Mehdi-Aiyad@users.noreply.github.com>
2023-06-30 16:35:33 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -14,9 +14,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			fallback: 'index.html'
-			}),
+		adapter: adapter(),
 
 		paths: {
 			base: process.env.APP_BASE || "",
